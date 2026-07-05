6f7bf3e475cb475424e3c99f42f7357e6c618e4a
devdasx <devdas98x@gmail.com>
2026-07-06 06:03:42 +0700

    Bump app version to 2.4.10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,8 +37,8 @@ android {
         applicationId = "com.satra.wallet"
         minSdk = 26
         targetSdk = 37
-        versionCode = 2
-        versionName = "0.1.1"
+        versionCode = 20410
+        versionName = "2.4.10"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
