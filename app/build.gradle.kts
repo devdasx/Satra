8cf99b47e04fda2e7ffcc1f042930565cc2e2d81
devdasx <devdas98x@gmail.com>
2026-07-06 03:33:58 +0700

    Build Play package as com.satra.wallet

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,11 +8,11 @@ android {
     compileSdk = 37
 
     defaultConfig {
-        applicationId = "dev.satra.wallet"
+        applicationId = "com.satra.wallet"
         minSdk = 26
         targetSdk = 37
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.1.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
